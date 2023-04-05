376ddaf3b5e60786295df3bbc797faee50af5008
prabinzz <prabinparajuli92@gmail.com>
2023-04-05 09:23:15 +0545

    Text reformate

--- components/DisplayQnA.js
+++ components/DisplayQnA.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const DisplayQnA = () => {
 	return (
-		<div className="">
+		<div className="leading-normal  text-c-semi-light">
 			<div>
-				<h1 className="text-3xl font-bold">Nepali Movie Database (NMDb)</h1>
-				<p className="mt-4 mb-8 text-c-semi-light">
+				<h1 className="text-2xl text-white cursor-vertical-text font-medium">
+					Nepali Movie Database (NMDb)
+				</h1>
+				<p className="mt-2 mb-8">
 					Did you know that the Nepali movie industry is growing rapidly, with
 					more and more people searching for Nepali movie related topics every
 					day? Just like the popularity of anime worldwide, Nepali movies are
 					becoming increasingly popular, which has led to a rise in the number
 					of Nepali movie streaming sites.
 				</p>
-				<p className="mt-4 mb-8 text-c-semi-light">
+				<p className="mt-2 mb-8">
 					At Nepali Movie Database (NMDB), we understand that not all Nepali
 					movie streaming sites are created equally. That's why we have created
 					NMDB to be the best Nepali movie database exclusively for Nepali
@@ -21,8 +23,8 @@ const DisplayQnA = () => {
 				</p>
 			</div>
 			<div>
-				<h1 className="text-2xl font-bold">1/ What is NMDB?</h1>
-				<p className="mt-4 mb-8 text-c-semi-light">
+				<h1 className="text-2xl text-white font-medium">1/ What is NMDB?</h1>
+				<p className="mt-2 mb-8">
 					NMDB (Nepali Movie Database) is a free online platform exclusively
 					dedicated to Nepali movies. It provides movie enthusiasts with a vast
 					collection of Nepali movies, from classic to modern, including the
@@ -31,47 +33,47 @@ const DisplayQnA = () => {
 				</p>
 			</div>
 			<div>
-				<h1 className="text-2xl font-bold">2/ Is NMDB safe?</h1>
-				<p className="mt-4 mb-8 text-c-semi-light">
+				<h1 className="text-2xl text-white font-medium">2/ Is NMDB safe?</h1>
+				<p className="mt-2 mb-8">
 					Yes, NMDB is completely safe. We ensure the safety of our users by
 					providing a secure and ad-free platform. We do not display any
 					suspicious ads and constantly monitor the site to maintain its safety.
 				</p>
 			</div>
 			<div>
-				<h1 className="text-2xl font-bold">
+				<h1 className="text-2xl text-white font-medium">
 					3/ What makes NMDB the best site for Nepali movie streaming?
 				</h1>
-				<p className="mt-4 mb-8 text-c-semi-light">
+				<p className="mt-2 mb-8">
 					NMDB is the best site for Nepali movie streaming due to the following
 					reasons:
 				</p>
-				<ul className="text-c-semi-light">
+				<ul className="">
 					<li className="mt-4">
-						<span className="font-medium">Content library:</span> NMDB has a
-						vast collection of Nepali movies, ranging from classic to modern,
-						and including the latest releases. You can find movies from
-						different genres, such as action, drama, comedy, romance, horror,
-						and many more.
+						<span className="font-bold">Content library:</span> NMDB has a vast
+						collection of Nepali movies, ranging from classic to modern, and
+						including the latest releases. You can find movies from different
+						genres, such as action, drama, comedy, romance, horror, and many
+						more.
 					</li>
 					<li className="mt-4">
-						<span className="font-medium">User interface:</span> NMDB has a
+						<span className="font-bold">User interface:</span> NMDB has a
 						user-friendly interface that is easy to navigate. You can search for
 						movies via the search box or browse through different categories to
 						find your desired movie.
 					</li>
 					<li className="mt-4">
-						<span className="font-medium">Device compatibility:</span> NMDB is
+						<span className="font-bold">Device compatibility:</span> NMDB is
 						compatible with both mobile and desktop devices, allowing you to
 						stream movies on any device you prefer.
 					</li>
 					<li className="mt-4">
-						<span className="font-medium">Updates:</span> We update our movie
+						<span className="font-bold">Updates:</span> We update our movie
 						collection regularly and add new releases as soon as they are
 						available.
 					</li>
 					<li className="mt-4">
-						<span className="font-medium">Customer care:</span> We provide 24/7
+						<span className="font-bold">Customer care:</span> We provide 24/7
 						customer support and promptly address any issues or queries that our
 						users might have.
 					</li>
